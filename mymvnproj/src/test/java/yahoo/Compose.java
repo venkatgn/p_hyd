c837f9ac132eeb53b51cf2eec57d1e3504e1c952
venkatgn <venkatadrin.g@gmail.com>
2015-11-27 14:43:13 +0530

    second time

--- mymvnproj/src/test/java/yahoo/Compose.java
+++ mymvnproj/src/test/java/yahoo/Compose.java
@@ -27,6 +27,7 @@ public class Compose
 	public void sendmail() throws Exception
 	{
 		Thread.sleep(5000);
+		System.out.println("This is to send mails");
         driver.findElement(By.xpath("//*[@class='composeicon']")).click();
         Thread.sleep(3000);
         driver.findElement(By.id("to")).sendKeys("abcd@gmail.com");
